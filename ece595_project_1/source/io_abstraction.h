aab7d1eaa71a508a0e3fb4c2a7da36e894295e8d
Devin Jaenicke <djaenicke.92@gmail.com>
2019-02-09 18:11:18 -0500

    Missed from previous commit

--- ece595_project_1/source/io_abstraction.h
+++ ece595_project_1/source/io_abstraction.h
@@ -14,6 +14,27 @@ typedef enum
 
 typedef struct
 {
+    PORT_Type *pbase;
+    GPIO_Type *gbase;
+    uint32_t   pin;
+    port_mux_t mux;
+    gpio_pin_direction_t dir;
+    GPIO_State_T init_state;
+} Pin_Cfg_T;
+
+typedef enum
+{
+    MOTOR_STEP_CTRL=0,
+    MOTOR_DIR_CTRL,
+    MOTOR_SLEEP,
+    MOTOR_DRV_EN,
+    MOTOR_DRV_RESET,
+    BLUE_LED,
+    NUM_IO /* !!! Make sure this is last */
+} IO_Map_T;
+
+extern const Pin_Cfg_T Pin_Cfgs[NUM_IO];
+
 extern void Set_GPIO(IO_Map_T gpio, GPIO_State_T state);
 extern uint32_t Read_GPIO(IO_Map_T gpio);
 
