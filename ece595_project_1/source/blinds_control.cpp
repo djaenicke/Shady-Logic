bb02da20b1eb9bbdbbeeee41165fd51d1b09e5c7
Devin Jaenicke <djaenicke.92@gmail.com>
2019-02-10 19:06:10 -0500

    Added temporary manual OPEN/CLOSE control

--- ece595_project_1/source/blinds_control.cpp
+++ ece595_project_1/source/blinds_control.cpp
@@ -16,10 +16,18 @@ typedef enum
     REQUEST_CLOSED_POSITION,
     SEEK_OPEN_POSITION,
     POSITION_LEARNED,
+    MANUAL_CONTROL,
     LIGHT_CONTROL,
     TIME_CONTROL,
     IDLE
 } Control_State_T;
+
+typedef struct Position_Tag
+{
+    uint8_t fully_open;
+    uint8_t fully_closed;
+    uint8_t current;
+} Position_T;
 
 
 uint8_t UART_RX_Buffer[4];
@@ -28,7 +36,8 @@ char UART_TX_Buffer[128];
 uart_rtos_handle_t Handle;
 struct _uart_handle T_Handle;
 
-uart_rtos_config_t uart_config = {
+uart_rtos_config_t uart_config =
+{
     UART0,
     0,
     115200,
@@ -40,14 +49,14 @@ uart_rtos_config_t uart_config = {
 
 static volatile Control_State_T Ctrl_State;
 static Motor Stepper_Motor;
-
-static float Closed_Position;
-static float Open_Position;
+static Position_T Blinds_Position = {0};
+static bool Toggle_State = false;
 
 static volatile bool Print_Closed_Pos_Info = false;
 static volatile bool Print_Open_Pos_Info = false;
 
 void Run_Position_Learning(void);
+void Boolean_Position_Control(void);
 
 extern "C"
 {
@@ -63,6 +72,14 @@ void PORTC_IRQHandler(void)
     {
         Ctrl_State = POSITION_LEARNED;
     }
+    else if (MANUAL_CONTROL == Ctrl_State)
+    {
+        Toggle_State = true;
+    }
+    else
+    {
+        /* Do Nothing */
+    }
 
     PORT_ClearPinsInterruptFlags(PORTC, 0xFFFFFFFF);
 }
@@ -71,13 +88,14 @@ void PORTC_IRQHandler(void)
 void Init_Blinds_Control(void)
 {
     Ctrl_State = POSITION_NOT_LEARNED;
-    Stepper_Motor.Enable_Driver();
+    Stepper_Motor.Disable_Driver();
+    Stepper_Motor.Sleep();
     BOARD_Enable_SW_Interrupts();
 
     /* Initialize a UART instance */
     uart_config.srcclk = CLOCK_GetFreq(UART0_CLK_SRC);
     UART_RTOS_Init(&Handle, &T_Handle, &uart_config);
-    sprintf(UART_TX_Buffer, "Blinds controller initialized.\r\n\n");
+    sprintf(UART_TX_Buffer, "Blinds controller initialized. Press SW3 to start position learning procedure.\r\n\n");
     UART_RTOS_Send(&Handle, (uint8_t *)UART_TX_Buffer, strlen(UART_TX_Buffer));
 }
 
@@ -117,13 +135,15 @@ void Ctrl_State_Machine(void)
                 sprintf(UART_TX_Buffer, "Place blinds in fully closed position and press SW3 when finished.\r\n\n");
                 UART_RTOS_Send(&Handle, (uint8_t *)UART_TX_Buffer, strlen(UART_TX_Buffer));
                 Stepper_Motor.Zero_Position();
-                Closed_Position = Stepper_Motor.Get_Position();
+                Blinds_Position.fully_closed = (uint8_t) Stepper_Motor.Get_Position();
                 Print_Closed_Pos_Info = false;
             }
             break;
         case SEEK_OPEN_POSITION:
             if (Print_Open_Pos_Info)
             {
+                Stepper_Motor.Enable_Driver();
+                Stepper_Motor.Wakeup();
                 sprintf(UART_TX_Buffer, "Closed position learned!\r\nPress SW3 to begin seeking for open position.\r\n"
                                          "When fully open, press SW2 to finish the procedure.\r\n\n");
                 UART_RTOS_Send(&Handle, (uint8_t *)UART_TX_Buffer, strlen(UART_TX_Buffer));
@@ -133,14 +153,56 @@ void Ctrl_State_Machine(void)
         case POSITION_LEARNED:
             sprintf(UART_TX_Buffer, "Position learned!\r\n");
             UART_RTOS_Send(&Handle, (uint8_t *)UART_TX_Buffer, strlen(UART_TX_Buffer));
-            Open_Position = Stepper_Motor.Get_Position();
+            Blinds_Position.fully_open = (uint8_t) Stepper_Motor.Get_Position();
+            Blinds_Position.current = Blinds_Position.fully_open;
             Stepper_Motor.Sleep();
-            Ctrl_State = IDLE;
+            Ctrl_State = MANUAL_CONTROL; /* !!! TEMPORARY  !!!*/
             break;
         case IDLE:
         case LIGHT_CONTROL:
         case TIME_CONTROL:
             break;
+        case MANUAL_CONTROL:
+            if (Toggle_State)
+            {
+                Boolean_Position_Control();
+            }
+            Toggle_State = false;
+            break;
+    }
+}
+
+void Boolean_Position_Control(void)
+{
+    float deg_change;
+
+    if (Blinds_Position.current == Blinds_Position.fully_open)
+    {
+        /* Close */
+        sprintf(UART_TX_Buffer, "Closing blinds...\r\n");
+        UART_RTOS_Send(&Handle, (uint8_t *)UART_TX_Buffer, strlen(UART_TX_Buffer));
+
+        deg_change = (float) Blinds_Position.fully_closed - Blinds_Position.fully_open;
+        Stepper_Motor.Wakeup();
+        Stepper_Motor.Rotate(deg_change);
+        Stepper_Motor.Sleep();
+
+        Blinds_Position.fully_closed = (uint8_t) Stepper_Motor.Get_Position();
+        Blinds_Position.current = Blinds_Position.fully_closed;
+    }
+    else if (Blinds_Position.current == Blinds_Position.fully_closed)
+    {
+        /* Open */
+        sprintf(UART_TX_Buffer, "Opening blinds...\r\n");
+        UART_RTOS_Send(&Handle, (uint8_t *)UART_TX_Buffer, strlen(UART_TX_Buffer));
+
+        deg_change = Blinds_Position.fully_open - Blinds_Position.fully_closed;
+        Stepper_Motor.Wakeup();
+        Stepper_Motor.Rotate(deg_change);
+        Stepper_Motor.Sleep();
+
+        Blinds_Position.fully_open = (uint8_t) Stepper_Motor.Get_Position();
+        Blinds_Position.current = Blinds_Position.fully_open;
     }
 }
 
